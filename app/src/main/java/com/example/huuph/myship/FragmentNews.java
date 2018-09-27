5c56cff225dd70a677590ffa93e5bd5dcd00380a
vutienviet <42883535+vutienviet@users.noreply.github.com>
2018-09-27 13:50:55 +0700

    giao diện cho main chính

---
+++ app/src/main/java/com/example/huuph/myship/FragmentNews.java
@@ -0,0 +1,39 @@
+package com.example.huuph.myship;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentNews extends Fragment {
+    private static FragmentNews instance;
+    private TextView tvNumber;
+    private int number = 0;
+
+    public static FragmentNews getInstance() {
+        if (instance == null) {
+            instance = new FragmentNews();
+        }
+        return instance;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.ui_news,container,false);
+        return view;
+    }
+
+    private void innit() {
+        tvNumber = getView().findViewById(R.id.tv_number);
+    }
+
+    public void setNumber() {
+        number++;
+        tvNumber.setText(number + "");
+    }
+}
