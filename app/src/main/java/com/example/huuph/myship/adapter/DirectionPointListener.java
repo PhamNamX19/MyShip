88bc7506babfde78f05d0957931779bbab67bdd4
zeroc <zerocute12@hotmail.com>
2018-12-06 20:39:17 +0700

    anhanh

---
+++ app/src/main/java/com/example/huuph/myship/adapter/DirectionPointListener.java
@@ -0,0 +1,9 @@
+package com.example.huuph.myship.adapter;
+
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public interface DirectionPointListener {
+
+     void onPath(PolylineOptions polyLine);
+
+}
